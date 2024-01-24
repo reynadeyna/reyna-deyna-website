18ac0e55b7d01fb3502ffd076c4e82d574da3f94
Angel Reyna <rugile@protonmail.com>
2024-01-25 00:35:37 +0200

    footeriux

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div className="flex flex-col h-screen overflow-x-hidden">
       <div className="flex flex-col items-center justify-center h-screen">
         <div className="p-0 w-90 mx-auto">
-          <div className="widget text-center mt-3 p-3">
+          <div className="widget text-center mt-20">
             <Link to="/delufesto" className="lg:text-2xl">
               <h1>Greetings, Angel</h1>
             </Link>
@@ -14,7 +14,7 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="flex flex-col items-center justify-center mb-3">
+      <div className="footer flex flex-col items-center justify-center mb-3">
         <h2 className="footer-text text-center">Designed and built by Angelware</h2>
 
         <div className="flex space-x-2 my-2 p-1">
