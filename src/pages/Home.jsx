8c7b0959f2541f4d599ca49fb1b7443e15aca258
Angel Reyna <rugile@protonmail.com>
2024-01-29 22:31:23 +0200

    home fixed flex

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,9 +23,9 @@ const Home = () => {
 
   return (
     <div className="flex flex-col h-screen overflow-x-hidden">
-      <div className="flex flex-col items-center justify-center h-screen">
-        <div className="p-0 w-90 mx-auto mt-10">
-          <div className="explore text-center mt-20">
+      <div className="flex flex-col items-center justify-center h-screen fixed top-0 left-0 right-0 z-10">
+        <div className="p-0 w-90 mx-auto">
+          <div className="explore text-center">
             <motion.div
               animate={controls}
               initial={{ scale: 1, opacity: 1 }}
@@ -45,8 +45,8 @@ const Home = () => {
                   style={{
                     filter: "brightness(0) contrast(1.2) saturate(7)", 
                     opacity: 0.6,
-                    width: "500px",
-                    height: "500px",
+                    width: "300px",
+                    height: "300px",
                   }}
                 />
               </Link>
@@ -55,13 +55,15 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="footer flex flex-col items-center justify-center mb-3">
-        <h2 className="footer-text text-center">
-          Designed and built by Angelware
-        </h2>
+      <div className="fixed bottom-0 left-0 right-0 z-10">
+        <div className="footer flex flex-col items-center justify-center mb-3">
+          <h2 className="footer-text text-center">
+            Designed and built by Angelware
+          </h2>
 
-        <div className="flex space-x-2 my-2 p-1">
-          <Links />
+          <div className="flex space-x-2 my-2 p-1">
+            <Links />
+          </div>
         </div>
       </div>
     </div>
