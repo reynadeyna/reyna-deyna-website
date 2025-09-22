6e32838c9cc2a377b0db2a991bc8423fa26288a4
Angel Reyna <rugile@protonmail.com>
2025-09-22 20:24:57 +0300

    links

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -13,67 +13,33 @@ const Links = () => {
   return (
     <div className="flex flex-wrap justify-center space-x-6 underline decoration-wavy decoration-zinc-300  ">
       <a
-        href="https://amzina.lt/links"
+        href=""
         target="_blank"
         className="hover:animate-pulse"
       >
         {" "}
-        listen
-        {/* <img src={soundcloud} className="logo" alt="SoundCloud logo" /> */}
+        tickets
       </a>
 
       <a
-        href="https://www.youtube.com/@amzinaximus"
+        href=""
         target="_blank"
         className="hover:animate-pulse"
       >
         {" "}
-        watch
-        {/* <img src={youtube} className="logo" alt="YouTube logo" /> */}
+        music
       </a>
 
       <a
-        href="https://www.youtube.com/@amzinaximus"
+        href=""
         target="_blank"
         className="hover:animate-pulse"
       >
         {" "}
         shop
-        {/* <img src={youtube} className="logo" alt="YouTube logo" /> */}
       </a>
 
-      {/* <a href=" https://matrix.to/#/@reynadeyna:matrix.org?client=im.fluffychat" target="_blank" className="hover:animate-pulse"> Matrix
-      </a> */}
 
-      {/* < <a href="https://bsky.app/profile/reynadeyna.bsky.social" target="_blank" className="hover:animate-pulse"> Bluesky
-        <img src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Bluesky_Logo.svg" className="logo black-svg" alt="bluesky logo" />
-        <img src={bluesky} className="logo" alt="Bluesky logo" />
-      </a> */}
-
-      {/* <a href="https://open.spotify.com/artist/2UwsiwBg4Tu7vszP2WjkjC" target="_blank"> Spotify
-        <img src={spotify} className="logo" alt="Spotify logo" />
-      </a> */}
-
-      {/* <a href="https://reynadeyna.bandcamp.com" target="_blank">
-        <img src={bandcamp} className="logo" alt="bandcamp logo" />
-      </a> */}
-
-      {/* <a href="https://discord.gg/QRY43edW" target="_blank" className="hover:animate-pulse"> Discord
-              <img src={discord} className="logo" alt="Discord logo" />
-            </a> */}
-
-      {/* <a href="https://www.instagram.com/reynadeyna" target="_blank">
-        <img src={instagram} className="logo" alt="Instagram logo" />
-      </a> */}
-
-      {/* <a href="https://www.tiktok.com/@reynadeyna" target="_blank">
-        <img src={tiktok} className="logo" alt="TikTok logo" />
-      </a> */}
-
-      {/* 
-      <a href="https://github.com/reynadeyna" target="_blank" className="hover:animate-pulse"> Github
-        <img src={octocat} className="logo" alt="Github logo" />
-      </a> */}
     </div>
   );
 };
