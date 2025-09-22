29a27bf0a02ded3ea844ab8ed1913603433d26fb
Angel Reyna <rugile@protonmail.com>
2025-09-22 21:17:04 +0300

    music

--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -7,8 +7,6 @@ const Music = () => {
         <div className="flex flex-col items-center justify-center text-center mt-12 gap-16">
           {/* Widget 1 */}
           <div className="widget w-full  border border-gray-300 p-5">
-
-
             <div className="aspect-w-16 aspect-h-9">
               <iframe
                 src="https://www.youtube.com/embed/izSlIxitGUg"
@@ -18,20 +16,14 @@ const Music = () => {
                 className="w-full h-full"
               ></iframe>
             </div>
-            <p className="text-[12px] leading-5 text-white mt-5">
-              Always alive in the fire, who said that I've died? I live eternal
-              life, I fall with grace, and then, I rise. Because I am hot like
-              Phoenix! I am lit by eternal glow and rising slowly...
-              <br />
-              Klaida sistemoje. Aš tau laidu išlysiu. Kas ji tokia? Tu pabandyk
-              išencryptint. Ji ištrina viską. Ji plika ir basa, bet amzina.lt
-              servery tvarka. Metų išpuolis, computer virus, Tity Pop Smoke.
-              Manęs čia per daug!
-              <br />
-              Aš važiuoju lėtai, kad matytų velnei, and no one does it like
-              me...
-              <br />
+            <p className="text-[10px] leading-4 text-white mt-5">
+              System Error I will reach you through the wire. Who is she? Try to
+              decrypt it. She erases everything. She’s naked and bare, but her
+              server is in order. Attack of the year, computer virus, Titty Pop
+              Smoke. There’s too much of me here. As vaziuoju letai kad matytu
+              velnei and no one does it like me.
             </p>
+            
             {/* W2 */}
           </div>
 
