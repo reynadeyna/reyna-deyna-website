f44076974229b074364bc0d06f4bc78a7f88fde1
Angel Reyna <rugile@protonmail.com>
2025-10-04 17:22:36 +0300

    blurry back

--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -1,176 +1,113 @@
 import React from "react";
 
 const Music = () => {
+  const widgets = [
+    {
+      type: "iframe",
+      src: "https://www.youtube.com/embed/izSlIxitGUg?controls=1&autoplay=1&mute=1&rel=0&modestbranding=1",
+      description:
+        "System Error I will reach you through the wire. Who Am I? Try to decrypt it. I erase everything. I am naked and bare, but my server is in order. Attack of the year, computer virus, Titty Pop Smoke. There’s too much of me here. As vaziuoju letai kad matytu velnei and no one does it like me.",
+    },
+    {
+      type: "img",
+      src: "https://f4.bcbits.com/img/a1794344270_10.jpg",
+      description: "Album, Xtraxeyna. Artwork by Jelena Luise",
+    },
+    {
+      type: "img",
+      src: "https://images.squarespace-cdn.com/content/v1/54faf78ce4b04da0abdfbde8/1620210338591-M9D00EAJIH39WMSTZ6GC/reyna_deyna_1612550545.jpg?format=2500w",
+      description:
+        "Interview, Coeval Magazine. Photography by Gedvile Tamosiunaite",
+      link: "https://www.coeval-magazine.com/coeval/reyna-deyna",
+    },
+    {
+      type: "img",
+      src: "https://f4.bcbits.com/img/a3817085623_10.jpg",
+      description: "Single, Say. Artwork by Jelena Luise",
+    },
+    {
+      type: "img",
+      src: "https://www.ore.lt/wordpress/wp-content/uploads/2018/10/Rugile_7-293x390.jpg",
+      description: "Interview, Ore.lt. Photography Jacopo Falsetta",
+      link: "https://www.ore.lt/2018/10/rugile-pas-muzika-del-drasos",
+    },
+    {
+      type: "img",
+      src: "https://images.squarespace-cdn.com/content/v1/54faf78ce4b04da0abdfbde8/1620210365475-Q0BDOMW6VK7DU467A1LG/reyna_deyna_1614103978_1.jpg?format=2500w",
+      description:
+        "Interview, Coeval Magazine. Photography by Gedvile Tamosiunaite",
+      link: "https://www.coeval-magazine.com/coeval/reyna-deyna",
+    },
+    {
+      type: "img",
+      src: "https://f4.bcbits.com/img/a3986550602_10.jpg",
+      description: "Single, Scam Angel. Artwork by Jelena Luise",
+    },
+    {
+      type: "img",
+      src: "https://images.squarespace-cdn.com/content/v1/54faf78ce4b04da0abdfbde8/1620210300747-FK25SBJWX4TE7U8POEZU/01_Gedvile_Tamosiunaite_Reyna_Deyna.jpg?format=2500w",
+      description:
+        "Interview, Coeval Magazine. Photography by Gedvile Tamosiunaite",
+      link: "https://www.coeval-magazine.com/coeval/reyna-deyna",
+    },
+    {
+      type: "img",
+      src: "https://f4.bcbits.com/img/a0630777698_10.jpg",
+      description: "Single, Xtra. Artwork by Jelena Luise",
+    },
+    {
+      type: "img",
+      src: "https://www.ore.lt/wordpress/wp-content/uploads/2018/10/Rugile_1-e1538597464889-293x390.jpg",
+      description: "Interview, Ore.lt. Photography Jacopo Falsetta",
+      link: "https://www.ore.lt/2018/10/rugile-pas-muzika-del-drasos",
+    },
+  ];
+
   return (
-    <div className="flex justify-center pt-40 pb-40 px-5 bg-black">
-      <div className="w-full md:w-4/5 lg:w-3/4 xl:w-2/3">
-        <div className="flex flex-col items-center justify-center text-center mt-12 gap-16">
-          <div className="widget w-full p-5">
-            <div className="w-full" style={{ height: "500px" }}>
-              <iframe
-                src="https://www.youtube.com/embed/izSlIxitGUg?controls=1&autoplay=1&mute=1&rel=0&modestbranding=1"
-                title="YouTube video"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                className="w-full h-full"
-              ></iframe>
-            </div>
-            <p className="text-[10px] leading-6 text-white mt-3">
-              System Error I will reach you through the wire. Who is she? Try to
-              decrypt it. She erases everything. She’s naked and bare, but her
-              server is in order. Attack of the year, computer virus, Titty Pop
-              Smoke. There’s too much of me here. As vaziuoju letai kad matytu
-              velnei and no one does it like me.
+    <div className="flex justify-center px-4 py-20 bg-black min-h-screen">
+      <div className="w-full md:w-4/5 lg:w-3/4 xl:w-2/3 flex flex-col gap-8">
+        {widgets.map((widget, i) => (
+          <div
+            key={i}
+            className="w-full p-5 bg-white/5 backdrop-blur-lg rounded-xl shadow-md flex flex-col items-center"
+          >
+            {widget.type === "iframe" && (
+              <div className="w-full" style={{ height: "500px" }}>
+                <iframe
+                  src={widget.src}
+                  title={`widget-${i}`}
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  className="w-full h-full rounded-lg"
+                ></iframe>
+              </div>
+            )}
+            {widget.type === "img" && (
+              <img
+                src={widget.src}
+                alt={`widget-${i}`}
+                className="w-full h-auto rounded-xl"
+              />
+            )}
+            <p className="text-[12px] leading-5 text-white mt-3 text-center">
+              {widget.link ? (
+                <>
+                  <a
+                    href={widget.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-lime-200 underline"
+                  >
+                    Interview,
+                  </a>{" "}
+                  {widget.description.split("Interview,")[1]}
+                </>
+              ) : (
+                widget.description
+              )}
             </p>
           </div>
-
-          <div className="widget w-full p-5">
-            <img
-              src="https://f4.bcbits.com/img/a1794344270_10.jpg"
-              alt="Xtraxeyna Album Cover"
-              className="w-full h-auto rounded-md"
-            />
-            <p className="text-[10px] leading-5 text-white mt-2">
-              Album, Xtraxeyna
-              <br />
-              Artwork by Jelena Luise
-            </p>
-          </div>
-
-          <div className="widget w-full p-5">
-            <img
-              src="https://images.squarespace-cdn.com/content/v1/54faf78ce4b04da0abdfbde8/1620210338591-M9D00EAJIH39WMSTZ6GC/reyna_deyna_1612550545.jpg?format=2500w"
-              alt="Photo 3"
-              className="w-full h-auto rounded-md"
-            />
-            <p className="text-[10px] leading-5 text-white mt-2">
-              <a
-                href="              https://www.coeval-magazine.com/coeval/reyna-deyna
-"
-                className=" text-lime-200"
-              >
-                Interview,
-              </a>
-              Coeval Magazine,
-              <br />
-              Photography by Gedvile Tamosiunaite
-            </p>
-          </div>
-
-          <div className="widget w-full p-5">
-            <img
-              src="https://f4.bcbits.com/img/a3817085623_10.jpg"
-              alt="Single Cover 3"
-              className="w-full h-auto rounded-md"
-            />
-            <p className="text-[10px] leading-5 text-white mt-2">
-              Single, Say <br />
-              Artwork by Jelena Luise
-            </p>
-          </div>
-
-          <div className="widget w-full p-5">
-            <img
-              src="https://www.ore.lt/wordpress/wp-content/uploads/2018/10/Rugile_7-293x390.jpg"
-              alt="ORE 2"
-              className="w-full h-auto rounded-md"
-            />
-            <p className="text-[10px] leading-5 text-white mt-2">
-              <a
-                href="https://www.ore.lt/2018/10/rugile-pas-muzika-del-drasos"
-                className="underline text-lime-200"
-              >
-                Interview,
-              </a>
-              Ore.lt
-              <br />
-              Photography Jacopo Falsetta
-            </p>
-          </div>
-
-          <div className="widget w-full p-5">
-            <img
-              src="https://images.squarespace-cdn.com/content/v1/54faf78ce4b04da0abdfbde8/1620210365475-Q0BDOMW6VK7DU467A1LG/reyna_deyna_1614103978_1.jpg?format=2500w"
-              alt="Photo 2"
-              className="w-full h-auto rounded-md"
-            />
-            <p className="text-[10px] leading-5 text-white mt-2">
-              <a
-                href="              https://www.coeval-magazine.com/coeval/reyna-deyna
-"
-                className=" text-lime-200"
-              >
-                Interview,
-              </a>
-              Coeval Magazine,
-              <br />
-              Photography by Gedvile Tamosiunaite
-            </p>
-          </div>
-
-          <div className="widget w-full p-5">
-            <img
-              src="https://f4.bcbits.com/img/a3986550602_10.jpg"
-              alt="Single Cover 2"
-              className="w-full h-auto rounded-md"
-            />
-            <p className="text-[10px] leading-5 text-white mt-2">
-              Single, Scam Angel <br />
-              Artwork by Jelena Luise
-            </p>
-          </div>
-
-          <div className="widget w-full p-5">
-            <img
-              src="https://images.squarespace-cdn.com/content/v1/54faf78ce4b04da0abdfbde8/1620210300747-FK25SBJWX4TE7U8POEZU/01_Gedvile_Tamosiunaite_Reyna_Deyna.jpg?format=2500w"
-              alt="Photo 1"
-              className="w-full h-auto rounded-md"
-            />
-            <p className="text-[10px] leading-5 text-white mt-2">
-              <a
-                href="              https://www.coeval-magazine.com/coeval/reyna-deyna
-"
-                className=" text-lime-200"
-              >
-                Interview,
-              </a>
-              Coeval Magazine,
-              <br />
-              Photography by Gedvile Tamosiunaite
-            </p>
-          </div>
-
-          <div className="widget w-full p-5">
-            <img
-              src="https://f4.bcbits.com/img/a0630777698_10.jpg"
-              alt="Single Cover 1"
-              className="w-full h-auto rounded-md"
-            />
-            <p className="text-[10px] leading-5 text-white mt-2">
-              Single, Xtra <br />
-              Artwork by Jelena Luise
-            </p>
-          </div>
-
-          <div className="widget w-full p-5">
-            <img
-              src="https://www.ore.lt/wordpress/wp-content/uploads/2018/10/Rugile_1-e1538597464889-293x390.jpg"
-              alt="ORE 1"
-              className="w-full h-auto rounded-md"
-            />
-            <p className="text-[10px] leading-5 text-white mt-2">
-              <a
-                href="https://www.ore.lt/2018/10/rugile-pas-muzika-del-drasos"
-                className="underline text-lime-200"
-              >
-                Interview,
-              </a>
-              Ore.lt
-              <br />
-              Photography Jacopo Falsetta
-            </p>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
