cf91d2bb22e7cd9241a7d3e5f3615e6bb6a0b3b4
Angel Reyna <rugile@protonmail.com>
2025-10-04 17:37:19 +0300

    data

--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -6,7 +6,7 @@ const Music = () => {
       type: "iframe",
       src: "https://www.youtube.com/embed/izSlIxitGUg?controls=1&autoplay=1&mute=1&rel=0&modestbranding=1",
       description:
-        "System Error I will reach you through the wire. Who Am I? Try to decrypt it. I erase everything. I am naked and bare, but my server is in order. Attack of the year, computer virus, Titty Pop Smoke. There’s too much of me here. As vaziuoju letai kad matytu velnei and no one does it like me.",
+        "System Error I will reach you through the wire. Who Am I? Try to decrypt it. I erase everything. I am naked and bare, but my servers are in order. Attack of the year, computer virus, Titty Pop Smoke. There’s too much of me here. As vaziuoju letai kad matytu velnei and no one does it like me.",
     },
     {
       type: "img",
@@ -63,51 +63,62 @@ const Music = () => {
     },
   ];
 
-  return (
-    <div className="flex justify-center px-4 py-20 bg-black min-h-screen">
+return (
+    <div className="flex justify-center px-4 py-20 min-h-screen">
       <div className="w-full md:w-4/5 lg:w-3/4 xl:w-2/3 flex flex-col gap-8">
-        {widgets.map((widget, i) => (
-          <div
-            key={i}
-            className="w-full p-5 bg-white/5 backdrop-blur-lg rounded-xl shadow-md flex flex-col items-center"
-          >
-            {widget.type === "iframe" && (
-              <div className="w-full" style={{ height: "500px" }}>
-                <iframe
-                  src={widget.src}
-                  title={`widget-${i}`}
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                  className="w-full h-full rounded-lg"
-                ></iframe>
-              </div>
-            )}
-            {widget.type === "img" && (
-              <img
-                src={widget.src}
-                alt={`widget-${i}`}
-                className="w-full h-auto rounded-xl"
-              />
-            )}
-            <p className="text-[12px] leading-5 text-white mt-3 text-center">
-              {widget.link ? (
-                <>
-                  <a
-                    href={widget.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-lime-200 underline"
-                  >
-                    Interview,
-                  </a>{" "}
-                  {widget.description.split("Interview,")[1]}
-                </>
-              ) : (
-                widget.description
+        {widgets.map((widget, i) => {
+          const content = (
+            <>
+              {widget.type === "iframe" && (
+                <div className="w-full" style={{ height: "500px" }}>
+                  <iframe
+                    src={widget.src}
+                    title={`widget-${i}`}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    className="w-full h-full rounded-lg"
+                  ></iframe>
+                </div>
               )}
-            </p>
-          </div>
-        ))}
+              {widget.type === "img" && (
+                <img
+                  src={widget.src}
+                  alt={`widget-${i}`}
+                  className="w-full h-auto rounded-xl"
+                />
+              )}
+              <p className="text-[12px] leading-5 text-white mt-3 text-center">
+                {widget.link ? (
+                  <>
+                    <span className="underline"></span>{" "}
+                    {widget.description.split("Interview,")[1]}
+                  </>
+                ) : (
+                  widget.description
+                )}
+              </p>
+            </>
+          );
+
+          return widget.link ? (
+            <a
+              key={i}
+              href={widget.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-full p-5 bg-white/5 backdrop-blur-lg rounded-xl shadow-md flex flex-col items-center hover:scale-105 transition-transform duration-200"
+            >
+              {content}
+            </a>
+          ) : (
+            <div
+              key={i}
+              className="w-full p-5 bg-white/5 backdrop-blur-lg rounded-xl shadow-md flex flex-col items-center"
+            >
+              {content}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
